ca6ed6c3828298ae355baba8b43891269b2cf32c
dv9090 <vominhduy2014@gmail.com>
2025-08-10 22:38:20 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+
+
+# city = input("Enter your city that you want to check the weather for: ")
+
+
+# Weather_At(city)
+
+# def Weather_At(city):
+#     pass
+
+# try:
+
+# except Exception as e:
+#     print(f"An error occurred: {e}")
