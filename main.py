3969f9c1e9c678ca3acaeb2e3a87be17a98d17f3
dv9090 <vominhduy2014@gmail.com>
2025-08-12 22:57:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import requests
+from bs4 import BeautifulSoup
+
+
+base_url = "https://weather.com/weather/today/l/d14a70242c8d79bf622ef26e82a35eeafa1643baea3abb51c3c0290a1fa1998e"
+
+#city = input("Enter your city that you want to check the weather for: ")
+
+
+def Weather_At():
+    response = requests.get(base_url)
+
+    if response.status_code == 200:
+        print('Data retrieved successfully')
+        BeautifulSoup(response.text, 'html.parser')
+        
+
+    else:
+        print("Error fetching data from Weather.com API")
 
 
 
-# city = input("Enter your city that you want to check the weather for: ")
+    
 
-
-# Weather_At(city)
-
-# def Weather_At(city):
-#     pass
-
-# try:
-
-# except Exception as e:
-#     print(f"An error occurred: {e}")
+Weather_At()
