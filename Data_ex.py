c692ba66b140361e46246ed15f9dd880ee1052e7
dv9090 <vominhduy2014@gmail.com>
2025-08-15 14:12:01 -0400

    finish

--- Data_ex.py
+++ Data_ex.py
@@ -17,12 +17,13 @@ if response.status_code == 200:
     data = response.json()
     
     # Now you can work with the data
-    temperature = data['main']['temp']
+    temperature_C = round(data['main']['temp'])
+    temperature_F = round((temperature_C * 9/5) + 32)
     description = data['weather'][0]['description']
     humidity = data['main']['humidity']
     
-    print(f"Current weather in {city}:")
-    print(f"Temperature: {temperature}°C")
+    print(f"Current weather in {city}")
+    print(f"Temperature: {temperature_C}°C, {temperature_F}°F")
     print(f"Description: {description.capitalize()}")
     print(f"Humidity: {humidity}%")
 else:
